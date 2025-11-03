0a8a34607ffbc49f22197e04b31dcebfb752b685
ashwinpdahikar <ashwinpdahikar@gmail.com>
2025-11-03 19:57:51 +0530

    updated by ashwin dahikar

---
+++ index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <?php include 'include/header-section.php'; ?>
+   <!-- site metas -->
+   <title>Balaji Hotel And Lodge Chimur</title>
+</head>
+<!-- body -->
+
+<body class="main-layout">
+   <!-- loader  -->
+   <?php include 'include/loader.php'; ?>
+   <!-- end loader -->
+   <!-- header -->
+   <?php include 'include/header.php'; ?>
+   <!-- end header inner -->
+   <!-- end header -->
+   <!-- banner -->
+   <?php include 'include/banner.php'; ?>
+   <!-- end banner -->
+   <!-- about -->
+   <div class="about">
+      <div class="container-fluid">
+         <div class="row">
+            <div class="col-md-5">
+               <div class="titlepage">
+                  <h2>About Us</h2>
+                  <p>The passage experienced a surge in popularity during the 1960s when Letraset used it on their
+                     dry-transfer sheets, and again during the 90s as desktop publishers bundled the text with their
+                     software. Today it's seen all around the web; on templates, websites, and stock designs. Use our
+                     generator to get your own, or read on for the authoritative history of lorem ipsum. </p>
+                  <a class="read_more" href="Javascript:void(0)"> Read More</a>
+               </div>
+            </div>
+            <div class="col-md-7">
+               <div class="about_img">
+                  <figure><img src="images/about.png" alt="#" /></figure>
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+   <!-- end about -->
+   <!-- our_room -->
+  <?php
+   include 'include/our_room.php';
+   ?>
+   <!-- end our_room -->
+   <!-- gallery -->
+ <?php
+   include 'include/gallery.php';
+   ?>
+   <!-- end gallery -->
+   <!-- blog -->
+ <?php
+   include 'include/blog.php';
+   ?>
+   <!-- end blog -->
+   <!--  contact -->
+   <?php include 'include/contact.php'; ?>
+   <!-- end contact -->
+   <!--  footer -->
+   <?php include 'include/footer.php'; ?>
+   <!-- end footer -->
+   <?php include 'include/footer-section.php'; ?>
+</body>
+
+</html>
